1da84501d9b6df778a6da5789d3ccb219a2cc7b3
jir999 <jir999@users.noreply.github.com>
2021-02-28 21:20:31 +0400

    removing cosole.log

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const cache = new Map();
 
 export const cachableFetch = async (url) => {
     if (cache.has(url)) {
-        console.log("Cache WORKING");
         return cache.get(url);
     }
     else {
-        console.log("Fetch WORKING");
         const response = await fetch(url);
         const data = await response.json();
         cache.set(url, data);
