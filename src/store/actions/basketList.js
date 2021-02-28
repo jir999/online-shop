610be67e52d2e64e9568ba675124896f23091cd6
jir999 <jir999@users.noreply.github.com>
2021-02-28 20:29:15 +0400

    adding Minus, Plus, Delete functionality in Basket

--- src/store/actions/basketList.js
+++ src/store/actions/basketList.js
@@ -1,17 +1,36 @@
 export const addToCartClick = 'addToCartClick';
 export const basketIconClick = 'basketIconClick';
 export const orderBtnClick = 'orderBtnClick';
+export const deleteBtnClick = 'deleteBtnClick';
+export const minusIconClick = 'minusIconClick';
+export const addIconClick = 'addIconClick';
 
-export const handleAddToCartClick = (currentMenuData, bool) => ({
+export const handleAddToCartClick = (currentMenuData) => ({
     type: addToCartClick,
     currentMenuData,
-    showBasket: bool,
 });
 
-export const handleBasketIconClick = (bool) => ({
+export const handleBasketIconClick = () => ({
     type: basketIconClick,
-    showBasket: bool,
 });
+
+export const handleMinusIconClick = (menuPrice, id) => ({
+    type: minusIconClick,
+    menuPrice,
+    id,
+});
+
+export const handleAddIconClick = (menuPrice, id) => ({
+    type: addIconClick,
+    menuPrice,
+    id,
+});
+
+export const handleDeleteBtnClick = (menuPrice, id) => ({
+    type: deleteBtnClick,
+    basketItemId: id,
+    menuPrice,
+})
 
 export const handleOrderBtnClick = () => ({
     type: orderBtnClick,
--- src/store/reducers/basketList.js
+++ src/store/reducers/basketList.js
@@ -1,16 +1,43 @@
-import { addToCartClick, basketIconClick, orderBtnClick } from '../actions/basketList';
+import { addToCartClick, basketIconClick, orderBtnClick, deleteBtnClick, minusIconClick, addIconClick } from '../actions/basketList';
 
 export const basketListData = (state = [], action) => {
-    const { type, currentMenuData, showBasket } = action;
+    const { type, currentMenuData, basketItemId, menuPrice, id } = action;
     switch (type) {
         case addToCartClick:
             const newState = [...state];
-            newState.push({ ...currentMenuData, showBasket });
-            console.log("newState", newState);
+            // newState.push({ ...currentMenuData, showBasket });
+            newState.push({ ...currentMenuData });
             return newState;
         //return [...state, currentMenuData, showBasket];
         case basketIconClick:
             return [];
+        case minusIconClick:
+            const minusedMenus = state.map((item) => {
+                if (item.menuId === id) {
+                    const newItem = { ...item };
+                    newItem.menuPrice -= menuPrice;
+                    return newItem;
+                }
+                return item;
+            });
+            return minusedMenus;
+        case addIconClick:
+            const addedMenus = state.map((item) => {
+                if (item.menuId === id) {
+                    const newItem = { ...item };
+                    newItem.menuPrice += menuPrice;
+                    return newItem;
+                }
+                return item;
+            });
+            return addedMenus;
+        case deleteBtnClick:
+            const filteredList = state.filter((menuItem) => {
+                const newItem = { ...menuItem };
+                newItem.menuPrice = menuPrice;
+                return newItem.menuId !== basketItemId;
+            });
+            return filteredList;
         case orderBtnClick:
             return [];
         default:
