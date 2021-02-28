1bddee8125c396115b22e91ed2dd164964d1256b
jir999 <jir999@users.noreply.github.com>
2021-02-28 20:26:07 +0400

    adding disabled value to each object of menus array

--- src/store/actions/menus.js
+++ src/store/actions/menus.js
@@ -1,6 +1,25 @@
 export const fetchMenu = "fetchMenu";
+export const addToCartDisableTrue = 'addToCartDisableTrue';
+export const addToCartDisableFalse = 'addToCartDisableFalse';
+export const addToCartDisableAllFalse = 'addToCartDisableAllFalse';
 
 export const fetchedMenuData = (data) => ({
     type: fetchMenu,
     fetchedData: data
+});
+
+export const toggleAddToCartDisableTrue = (clickedMenuId, disabledValue) => ({
+    type: addToCartDisableTrue,
+    clickedMenuId,
+    disabledValue,
+});
+
+export const toggleAddToCartDisableFalse = (clickedMenuId, disabledValue) => ({
+    type: addToCartDisableFalse,
+    clickedMenuId,
+    disabledValue,
+});
+
+export const toggleAddToCartDisableAllFalse = () => ({
+    type: addToCartDisableAllFalse,
 });
--- src/store/reducers/menus.js
+++ src/store/reducers/menus.js
@@ -1,10 +1,42 @@
 import { fetchMenu } from '../actions/menus';
+import { addToCartDisableTrue } from '../actions/menus';
+import { addToCartDisableFalse } from '../actions/menus';
+import { addToCartDisableAllFalse } from '../actions/menus';
 
 export const menuData = (state = [], action) => {
-    const { type, fetchedData } = action;
+    const { type, fetchedData, clickedMenuId, disabledValue } = action;
     switch (type) {
         case fetchMenu:
-            return fetchedData;
+            return fetchedData.map((menu) => {
+                const newMenu = { ...menu };
+                newMenu.addToCartDisabled = false;
+                return newMenu;
+            });
+        // return fetchedData;
+        case addToCartDisableAllFalse:
+            return state.map((menu) => {
+                const newMenu = { ...menu };
+                newMenu.addToCartDisabled = false;
+                return newMenu;
+            });
+        case addToCartDisableTrue:
+            console.log('true', disabledValue)
+            return state.map((menu) => {
+                const newMenu = { ...menu };
+                if (newMenu.id === clickedMenuId) {
+                    newMenu.addToCartDisabled = disabledValue;
+                }
+                return newMenu;
+            });
+        case addToCartDisableFalse:
+            console.log('false', disabledValue);
+            return state.map((menu) => {
+                const newMenu = { ...menu };
+                if (newMenu.id === clickedMenuId) {
+                    newMenu.addToCartDisabled = disabledValue;
+                }
+                return newMenu;
+            })
         default:
             return state;
     }
