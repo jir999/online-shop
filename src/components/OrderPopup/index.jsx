998295382740ae5d1790cb1ff54ac346bc35150e
jir999 <jir999@users.noreply.github.com>
2021-02-28 21:25:32 +0400

    setting disabled value to false on order click in OrderPopup

--- src/components/OrderPopup/index.jsx
+++ src/components/OrderPopup/index.jsx
@@ -1,17 +1,17 @@
+import useStyles from './style';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import Button from '@material-ui/core/Button';
-import { handleOrderBtnClick } from '../../store/actions/basketList';
-import { useDispatch } from 'react-redux';
-import useStyles from './style';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import { handleOrderBtnClick } from '../../store/actions/basketList';
+import { toggleAddToCartDisableAllFalse } from '../../store/actions/menus';
+
 
 const OrderPopup = () => {
     const classes = useStyles();
-
     const dispatch = useDispatch();
-
     const [open, setOpen] = useState(false);
 
     const handleOpen = () => {
@@ -20,7 +20,8 @@ const OrderPopup = () => {
 
     const handleClose = () => {
         setOpen(false);
-        dispatch(handleOrderBtnClick())
+        dispatch(handleOrderBtnClick());
+        dispatch(toggleAddToCartDisableAllFalse());
     };
 
     return (
@@ -42,8 +43,16 @@ const OrderPopup = () => {
             >
                 <Fade in={open}>
                     <div className={classes.paper}>
-                        <h2 id="transition-modal-title">Your order will be delivered in 20 minutes </h2>
-                        <Button color="primary" variant="outlined" onClick={handleClose}>Ok</Button>
+                        <h2 id="transition-modal-title">
+                            Your order will be delivered in 20 minutes
+                        </h2>
+                        <Button
+                            color="primary"
+                            variant="outlined"
+                            onClick={handleClose}
+                        >
+                            Ok
+                        </Button>
                     </div>
                 </Fade>
             </Modal>
