329132485702044de304689db6b719cf6d232b26
jir999 <jir999@users.noreply.github.com>
2021-02-28 21:28:58 +0400

    removing searchValue reducer from reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,5 @@
 import { restaurantsData } from './restaurants';
 import { menuData } from './menus';
-import { searchValue } from './searchValue';
 import { dropDownData } from './dropDownData';
 import { basketListData } from './basketList';
 import { dropDownValue } from './dropDownValue';
@@ -8,7 +7,6 @@ import { dropDownValue } from './dropDownValue';
 export default {
     restaurantsData,
     menuData,
-    searchValue,
     dropDownData,
     basketListData,
     dropDownValue,
