372c7fcb001534e80d1727aa9a9c454f219b228d
jir999 <jir999@users.noreply.github.com>
2021-02-28 20:31:16 +0400

    getting and setting disabled value to true

--- src/components/AddToCart/index.jsx
+++ src/components/AddToCart/index.jsx
@@ -1,14 +1,28 @@
+import { useDispatch } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import { useDispatch } from 'react-redux';
 import { handleAddToCartClick } from '../../store/actions/basketList';
-
+import { toggleAddToCartDisableTrue } from '../../store/actions/menus';
 
 const AddToCart = (menuData) => {
     const dispatch = useDispatch();
+    const { menuId, disabledValue } = menuData;
+
+    const handleBtnClick = () => {
+        dispatch(toggleAddToCartDisableTrue(menuId, true));
+        dispatch(handleAddToCartClick(menuData));
+    };
 
     return (
-        <Button onClick={() => dispatch(handleAddToCartClick(menuData, true))} variant="contained" color="primary" endIcon={<AddShoppingCartIcon />}>Add to Cart</Button>
+        <Button
+            disabled={disabledValue}
+            onClick={handleBtnClick}
+            variant="contained"
+            color="primary"
+            endIcon={<AddShoppingCartIcon />}
+        >
+            Add to Cart
+        </Button>
     )
 };
 
