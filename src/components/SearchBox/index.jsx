1d89f25554eec8bc6c8cf44c5be1df646de51eb6
jir999 <jir999@users.noreply.github.com>
2021-02-28 21:19:46 +0400

    getting search value as prop

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -1,12 +1,25 @@
 import { useDispatch } from 'react-redux';
-import { handleInputChange } from '../../store/actions/restaurants';
+// import { handleInputChange } from '../../store/actions/restaurants';
 import Input from '@material-ui/core/Input';
 
-const SearchBox = () => {
+const SearchBox = ({ value, onInputChange }) => {
     const dispatch = useDispatch();
+
+    const handleInputChange = (e) => {
+        onInputChange(e.target.value);
+    }
+
     return (
-        <Input className="searchBox" onChange={(e) => dispatch(handleInputChange(e.target.value))} placeholder="Search by name" />
+        <Input
+            className="searchBox"
+            onChange={handleInputChange}
+            placeholder="Search by name"
+            value={value}
+        />
     )
+    // return (
+    //     <Input className="searchBox" onChange={(e) => dispatch(handleInputChange(e.target.value))} placeholder="Search by name" />
+    // )
 };
 
 export default SearchBox;
